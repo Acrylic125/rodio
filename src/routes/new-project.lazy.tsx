e52eef6af4525d15a03c1a800297aa2686e3e4e6
Acrylic125 <acrylic125email@gmail.com>
2024-05-04 20:07:53 +0800

    feat: folder selection

--- src/routes/new-project.lazy.tsx
+++ src/routes/new-project.lazy.tsx
@@ -9,8 +9,12 @@ import {
 } from "@/components/ui/breadcrumb";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { invoke } from "@tauri-apps/api";
-import { createDir } from "@tauri-apps/api/fs";
+import { createDir, exists } from "@tauri-apps/api/fs";
+import { useMutation } from "@tanstack/react-query";
+import { open } from "@tauri-apps/api/dialog";
+import { appLocalDataDir } from "@tauri-apps/api/path";
+import { useState } from "react";
+import path from "path";
 
 // readDir("");
 
@@ -18,8 +22,26 @@ export const Route = createLazyFileRoute("/new-project")({
   component: NewProject,
 });
 
+function shouldUseProjectNameAsBasename(_path: string, projectName: string) {
+  const basename = path.basename(_path);
+  return basename === projectName || _path === "" || projectName === "";
+}
+
 function NewProject() {
   const navigate = useNavigate({ from: "/new-project" });
+  const createProjectMut = useMutation({
+    mutationFn: async ({ path, name }: { path: string; name: string }) => {
+      // const fullPath = path.join(dir, name);
+      if (await exists(path)) {
+        throw Error(`${path} already exists.`);
+      }
+      createDir(name, {
+        recursive: true,
+      });
+    },
+  });
+  const [projectName, setProjectName] = useState("");
+  const [projectPath, setProjectPath] = useState("");
 
   return (
     <main className="flex flex-col items-center bg-background w-screen h-svh">
@@ -46,9 +68,9 @@ function NewProject() {
           className="flex flex-col gap-4 md:gap-8 lg:gap-12"
           onSubmit={(e) => {
             e.preventDefault();
-            invoke("hello_world", { name: "World" })
-              // `invoke` returns a Promise
-              .then((response) => console.log(response));
+            // invoke("hello_world", { name: "World" })
+            //   // `invoke` returns a Promise
+            //   .then((response) => console.log(response));
             navigate({
               to: "/project/$path",
               params: { path: "helloworld" },
@@ -61,11 +83,78 @@ function NewProject() {
               <Input
                 placeholder="Project name (e.g. Bus Detector)"
                 className="h-12"
+                value={projectName}
+                onChange={(e) => {
+                  const newProjectName = e.target.value;
+                  setProjectName(newProjectName);
+                  const currentProjectPath = projectPath;
+                  if (
+                    shouldUseProjectNameAsBasename(
+                      currentProjectPath,
+                      projectName
+                    )
+                  ) {
+                    const selectedProjectPath = path.join(
+                      projectName === ""
+                        ? currentProjectPath
+                        : path.dirname(currentProjectPath),
+                      newProjectName
+                    );
+                    setProjectPath(selectedProjectPath);
+                  }
+                }}
               />
             </label>
+            <div></div>
             <label className="flex flex-col gap-2">
               <p>Project Path</p>
-              <Input type="file" className="h-12" />
+              <div className="w-full flex flex-row gap-2">
+                <div className="w-full flex flex-row">
+                  <Button
+                    onClick={async (e) => {
+                      e.preventDefault();
+                      const currentProjectPath = projectPath;
+                      const selected = await open({
+                        directory: true,
+                        multiple: false,
+                        defaultPath:
+                          currentProjectPath == ""
+                            ? await appLocalDataDir()
+                            : currentProjectPath,
+                      });
+                      if (!selected) return;
+                      let selectedProjectPath = Array.isArray(selected)
+                        ? selected[0]
+                        : selected;
+                      if (
+                        shouldUseProjectNameAsBasename(
+                          currentProjectPath,
+                          projectName
+                        )
+                      ) {
+                        selectedProjectPath = path.join(
+                          selectedProjectPath,
+                          projectName
+                        );
+                      }
+                      setProjectPath(selectedProjectPath);
+                    }}
+                    type="button"
+                    variant="secondary"
+                    className="h-full rounded-r-none"
+                  >
+                    Browse
+                  </Button>
+                  <Input
+                    placeholder="Browse project location"
+                    className="w-full h-12 rounded-l-none"
+                    aria-label="Project Directory Base Path"
+                    value={projectPath}
+                    onChange={(e) => setProjectPath(e.target.value)}
+                  />
+                </div>
+              </div>
+              {/* <Input type="file" className="h-12" /> */}
             </label>
           </div>
           <div className="flex flex-row gap-2">
