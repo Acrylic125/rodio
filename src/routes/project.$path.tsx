bc452741fe8625ae23639226e98e47abb0899049
Acrylic125 <acrylic125email@gmail.com>
2024-05-06 08:14:20 +0800

    feat: add current project state !wip

--- src/routes/project.$path.tsx
+++ src/routes/project.$path.tsx
@@ -4,10 +4,11 @@ import { useEffect, useRef, useState } from "react";
 import { cn, resolveError } from "@/lib/utils";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { FilterIcon, PlusIcon } from "lucide-react";
+import { FilterIcon, Loader2, PlusIcon } from "lucide-react";
 import ImagePreview from "@/components/project/image-preview";
-import { create } from "zustand";
+import { StoreApi, UseBoundStore, create } from "zustand";
 import { RodioProject } from "@/lib/rodio";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export const Route = createFileRoute("/project/$path")({
   component: Project,
@@ -37,42 +38,145 @@ const labels = [
   },
 ] as const;
 
-export const useCurrentProject = create((set) => {
-  return {
-    project: null satisfies null | RodioProject,
-    initStatus: {
-      state: "idle",
-    } satisfies
+export const useCurrentProject: UseBoundStore<
+  StoreApi<{
+    project: RodioProject | null;
+    initStatus:
       | {
           state: "idle" | "loading" | "success";
         }
       | {
           state: "error";
           message: string;
+        };
+    imagePaths: null | string[];
+    selectedImage: null | string;
+    selectImage: (path: string) => void;
+    getImages: () => Promise<
+      {
+        path: string;
+      }[]
+    >;
+    initProject: (path: string) => Promise<void>;
+  }>
+> = create((set) => ({
+  project: null,
+  imagePaths: null,
+  initStatus: {
+    state: "idle",
+  },
+  selectedImage: null,
+  selectImage(path: string) {
+    set({ selectedImage: path });
+  },
+  async getImages() {
+    if (this.project === null) {
+      return [];
+    }
+    return this.project.images.getImages();
+  },
+  async initProject(path: string) {
+    set({
+      initStatus: {
+        state: "loading",
+      },
+    });
+    try {
+      const project = new RodioProject(path);
+      await project.init();
+      set({ project, initStatus: { state: "success" } });
+    } catch (error) {
+      set({
+        initStatus: {
+          state: "error",
+          message: resolveError(error),
         },
-    async initProject(path: string) {
-      set({ initStatus: "loading" });
-      try {
-        const project = new RodioProject(path);
-        await project.init();
-        set({ project, initStatus: "success" });
-      } catch (error) {
-        set({ initStatus: "error", error: resolveError(error) });
-        throw error; // Rethrow so it can be caught by the caller.
-      }
-    },
-  };
-});
+      });
+      throw error; // Rethrow so it can be caught by the caller.
+    }
+  },
+}));
+
+function ImageList() {
+  const currentProject = useCurrentProject((state) => {
+    return {
+      getImages: state.getImages,
+      imagePaths: state.imagePaths,
+      selectedImage: state.selectedImage,
+      selectImage: state.selectImage,
+    };
+  });
+
+  if (currentProject.imagePaths === null) {
+    return (
+      <ul className="w-full p-2 select-none">
+        {new Array(5).fill(null).map((_, i) => (
+          <li key={i} className="p-1">
+            <Skeleton className="w-full h-8" />
+          </li>
+        ))}
+      </ul>
+    );
+  }
+
+  return (
+    <ul className="w-full p-2 select-none">
+      {currentProject.imagePaths.length > 0 ? (
+        currentProject.imagePaths.map((path) => (
+          <li
+            key={path}
+            className={cn(
+              "p-1 cursor-pointer truncate w-full transition ease-in-out duration-200",
+              {
+                "text-gray-50 dark:text-gray-950 bg-primary rounded-sm":
+                  path === currentProject.selectedImage,
+                "text-gray-700 dark:text-gray-300":
+                  path !== currentProject.selectedImage,
+              }
+            )}
+            onClick={() => currentProject.selectImage(path)}
+          >
+            {path.split("/").pop()}
+          </li>
+        ))
+      ) : (
+        <div className="w-full p-4 flex items-center justify-center">
+          <p>No images found</p>
+        </div>
+      )}
+    </ul>
+  );
+}
 
 function Project() {
-  // const { path } = Route.useParams();
-  const [currentPath, setCurrentPath] = useState(paths[0]);
-  // const canvasRef = useRef<HTMLCanvasElement>(null);
+  const { path } = Route.useParams();
+  const [currentPath, setCurrentPath] = useState<string | null>(null);
+  const currentProject = useCurrentProject((state) => {
+    return {
+      project: state.project,
+      initProject: state.initProject,
+    };
+  });
+  useEffect(() => {
+    if (currentProject.project === null) {
+      // currentProject.initProject(path);
+      // setCurrentPath(paths[0]);
+    }
+  }, [
+    currentProject.project,
+    currentProject.initProject,
+    path,
+    setCurrentPath,
+  ]);
 
   return (
     <main className="flex flex-col items-center bg-background w-screen h-svh">
       <nav className="flex flex-row items-center w-full h-12 border-b border-gray-300 dark:border-gray-700 px-3">
-        <h1>Rodio</h1>
+        {currentProject.project === null ? (
+          <Skeleton className="w-32 h-4" />
+        ) : (
+          <h1>{currentProject.project.config.values.name}</h1>
+        )}
       </nav>
       <div className="w-full h-[calc(100svh-3rem)] flex flex-row">
         <section className="w-full h-[calc(100svh-3rem)] max-w-64 overflow-auto relative">
@@ -88,27 +192,14 @@ function Project() {
               </Button>
             </div>
           </div>
-          <ul className="w-full p-2">
-            {paths.map((path) => (
-              <li
-                key={path}
-                className={cn(
-                  "p-1 cursor-pointer truncate w-full transition ease-in-out duration-200",
-                  {
-                    "text-gray-50 dark:text-gray-950 bg-primary rounded-sm":
-                      path === currentPath,
-                    "text-gray-700 dark:text-gray-300": path !== currentPath,
-                  }
-                )}
-                onClick={() => setCurrentPath(path)}
-              >
-                {path.split("/").pop()}
-              </li>
-            ))}
-          </ul>
+          <ImageList />
         </section>
         <div className="relative w-full h-full bg-black flex items-center justify-center border-x border-gray-300 dark:border-gray-700">
-          <ImagePreview currentPath={currentPath} />
+          {currentPath !== null ? (
+            <ImagePreview currentPath={currentPath} />
+          ) : (
+            <Loader2 className="animate-spin w-8 h-8" />
+          )}
         </div>
         <section className="w-full h-[calc(100svh-3rem)] max-w-64 overflow-auto relative">
           <h2 className="text-gray-500 font-medium p-2 border-b border-gray-300 dark:border-gray-700">
