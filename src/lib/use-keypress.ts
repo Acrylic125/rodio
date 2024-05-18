2d485211fb64d394bae6605c4062068a7e444656
Acrylic125 <acrylic125email@gmail.com>
2024-05-18 15:58:49 +0800

    refactor: clean up imports

--- src/lib/use-keypress.ts
+++ src/lib/use-keypress.ts
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 
 type KeyCode = "Escape" | "Backspace" | "ControlLeft" | "KeyW";
 
--- src/routes/project.$path.tsx
+++ src/routes/project.$path.tsx
@@ -1,5 +1,5 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useMemo } from "react";
 import { cn, resolveError } from "@/lib/utils";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
