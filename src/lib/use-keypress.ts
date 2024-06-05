583e58d80001f6f20f7d2a9f274902f345f20077
Acrylic125 <acrylic125email@gmail.com>
2024-06-05 17:36:34 +0800

    chore: deprecating

--- src/lib/use-keypress.ts
+++ src/lib/use-keypress.ts
@@ -1,12 +1,17 @@
-import { useEffect, useRef } from "react";
+import { RefObject, useEffect, useRef } from "react";
 
 type KeyCode = "Escape" | "Backspace" | "ControlLeft" | "KeyW";
 
-export function useKeyPress(callback: () => void, keyCodes: KeyCode[]): void {
+export function useKeyPress<T extends HTMLElement>(
+  callback: () => void,
+  keyCodes: KeyCode[],
+  ref?: RefObject<T>
+): void {
   const pressedKeysRef = useRef<Set<string>>(new Set());
 
   useEffect(() => {
-    const downHandler = (e: KeyboardEvent) => {
+    const downHandler = (e: Event) => {
+      if (!(e instanceof KeyboardEvent)) return;
       if ((keyCodes as string[]).includes(e.code)) {
         const newKeys = new Set(pressedKeysRef.current);
         newKeys.add(e.code);
@@ -16,18 +21,22 @@ export function useKeyPress(callback: () => void, keyCodes: KeyCode[]): void {
         pressedKeysRef.current = newKeys;
       }
     };
-    const upHandler = (e: KeyboardEvent) => {
+    const upHandler = (e: Event) => {
+      if (!(e instanceof KeyboardEvent)) return;
       if ((keyCodes as string[]).includes(e.code)) {
         const newKeys = new Set(pressedKeysRef.current);
         newKeys.delete(e.code);
         pressedKeysRef.current = newKeys;
       }
     };
-    window.addEventListener("keydown", downHandler);
-    window.addEventListener("keyup", upHandler);
+
+    const ele = ref === undefined ? document : ref.current;
+    if (ele === null) return;
+    ele.addEventListener("keydown", downHandler);
+    ele.addEventListener("keyup", upHandler);
     return () => {
-      window.removeEventListener("keydown", downHandler);
-      window.removeEventListener("keyup", upHandler);
+      ele.removeEventListener("keydown", downHandler);
+      ele.removeEventListener("keyup", upHandler);
     };
-  }, [pressedKeysRef, callback]);
+  }, [pressedKeysRef, ref, ref?.current, callback]);
 }
