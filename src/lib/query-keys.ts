9527ba2c5140629393cc0398089054c8956fcd34
Acrylic125 <acrylic125email@gmail.com>
2024-06-18 07:45:30 +0800

    feat: isKeyStartingWithAnd

--- src/lib/query-keys.ts
+++ src/lib/query-keys.ts
@@ -8,9 +8,27 @@ export function isKeyStartingWith(
   queryKey: QueryKey,
   startingWith: PartialQueryKey
 ) {
+  if (queryKey.length < startingWith.length) return false;
+
   if (typeof startingWith === "string") {
     if (queryKey.length === 0) return false;
     return queryKey[0] === startingWith;
   }
   return startingWith.every((key, index) => queryKey[index] === key);
+}
+
+export function isKeyStartingWithAnd(
+  queryKey: QueryKey,
+  startingWith: PartialQueryKey,
+  and: PartialQueryKey
+) {
+  let offsetCheckIndex =
+    typeof startingWith === "string" ? 1 : startingWith.length;
+  if (queryKey.length < offsetCheckIndex + and.length) return false;
+  if (!isKeyStartingWith(queryKey, startingWith)) return false;
+
+  if (typeof and === "string") {
+    return queryKey[offsetCheckIndex] === and;
+  }
+  return and.every((key, index) => queryKey[offsetCheckIndex + index] === key);
 }
